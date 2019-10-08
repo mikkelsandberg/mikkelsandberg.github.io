2469c6f1e08244cddb22a330cf0d8b9da33573a4
mikkelsandberg <mikkel.h.sandberg@gmail.com>
2019-10-08 15:32:48 -0700

    updating twitter profile url

--- precache-manifest.f8d2103790f468b263c27ea009e70135.js
+++ precache-manifest.f8d2103790f468b263c27ea009e70135.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4113b3d2efe18a522d5d3b81a03b9173",
+    "revision": "303d386461081b69944033c1b26da62c",
     "url": "/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.9522b9fe.chunk.css"
   },
   {
-    "revision": "0daefc384ea3b816885f",
-    "url": "/static/css/main.4d93a6d1.chunk.css"
+    "revision": "0dbe6cf37a3fb4b7c355",
+    "url": "/static/css/main.7c0e2251.chunk.css"
   },
   {
     "revision": "69c7fb0ce3dd3ebe63b0",
     "url": "/static/js/2.330a5852.chunk.js"
   },
   {
-    "revision": "0daefc384ea3b816885f",
-    "url": "/static/js/main.54d3eca0.chunk.js"
+    "revision": "0dbe6cf37a3fb4b7c355",
+    "url": "/static/js/main.b02452d2.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.85bcd912210472cee216ffaeb17db497.js"
+  "/precache-manifest.f8d2103790f468b263c27ea009e70135.js"
 );
 
 self.addEventListener('message', (event) => {
