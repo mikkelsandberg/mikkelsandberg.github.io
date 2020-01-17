e79d98015c1aa7038b43d1ae59cea56d0db8bd93
Mikkel Sandberg <mikkel.h.sandberg@gmail.com>
2020-01-17 14:25:12 -0800

    adding analytics to Basin

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f8d2103790f468b263c27ea009e70135.js"
+  "/precache-manifest.b4d553703aa9c0d8cdff32f43b98c70d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
