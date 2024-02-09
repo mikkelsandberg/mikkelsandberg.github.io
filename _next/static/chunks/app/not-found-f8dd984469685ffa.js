1142407aea87cd40c52f7b38073b2aeb2021b4f9
Mikkel Sandberg <mikkel@littlebook.studio>
2024-02-09 15:14:40 -0800

    adding nextjs version

---
+++ _next/static/chunks/app/not-found-f8dd984469685ffa.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[160],{4671:function(n,e,t){Promise.resolve().then(t.t.bind(t,40,23)),Promise.resolve().then(t.t.bind(t,4502,23)),Promise.resolve().then(t.t.bind(t,5250,23))},40:function(){},4502:function(){}},function(n){n.O(0,[250,971,69,744],function(){return n(n.s=4671)}),_N_E=n.O()}]);
---
+++ _next/static/qV-jRMZOldBok6VmMy7j4/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fmy-work\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
